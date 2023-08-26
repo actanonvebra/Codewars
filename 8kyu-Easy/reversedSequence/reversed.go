2f5506b63615d6c3f9b15588c65139c0f79764e8
actanonverba <96337247+actanonvebra@users.noreply.github.com>
2023-08-26 20:10:36 +0300

    Update reversed.go

--- 8kyu-Easy/reversedSequence/reversed.go
+++ 8kyu-Easy/reversedSequence/reversed.go
@@ -2,6 +2,11 @@ package main
 
 import "fmt"
 
+/*
+Build a function that returns an array of integers from n to 1 where n>0.
+
+Example : n=5 --> [5,4,3,2,1]
+*/
 func ReverseSeq(n int) []int {
 	arr := make([]int, n)
 	count := 0
