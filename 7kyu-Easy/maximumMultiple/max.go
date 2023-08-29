d6a8b87c8dfe8b004fcaddaae477e68d375e47f4
İbrahim Serhat Bulut <96337247+actanonvebra@users.noreply.github.com>
2023-08-29 17:09:45 +0300

    Update max.go

--- 7kyu-Easy/maximumMultiple/max.go
+++ 7kyu-Easy/maximumMultiple/max.go
@@ -2,6 +2,22 @@ package main
 
 import "fmt"
 
+/*
+Task
+Given a Divisor and a Bound , Find the largest integer N , Such That ,
+
+Conditions :
+N is divisible by divisor
+
+N is less than or equal to bound
+
+N is greater than 0.
+
+Notes
+The parameters (divisor, bound) passed to the function are only positive values .
+It's guaranteed that a divisor is Found .
+*/
+
 func MaxMultiple(d, b int) int {
 	var last int = 0
 	var add int = d
