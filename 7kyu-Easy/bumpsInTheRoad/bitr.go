476dc92ed20be17adec3741726a60f090092a139
İbrahim Serhat Bulut <96337247+actanonvebra@users.noreply.github.com>
2023-09-14 13:27:17 +0300

    Update bitr.go

--- 7kyu-Easy/bumpsInTheRoad/bitr.go
+++ 7kyu-Easy/bumpsInTheRoad/bitr.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
+/*
+Your car is old, it breaks easily. The shock absorbers are gone and you think it can handle about 15 more bumps before it dies totally.
+Unfortunately for you, your drive is very bumpy! Given a string showing either flat road (_) or bumps (n).
+If you are able to reach home safely by encountering 15 bumps or less, return Woohoo!, otherwise return Car Dead
+*/
+
 func Bump(s string) string {
 	bumpsCount := 0
 	for i := 0; i < len(s); i++ {
